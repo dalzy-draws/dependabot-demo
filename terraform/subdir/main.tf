a5abee9bb20bc52ed57aed7545ce4a39125543d7
Dalzy Mendoza <dalzy@canva.com>
2024-05-01 10:27:33 +1000

    add terraform/subdir

---
+++ terraform/subdir/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 0.14.11, < 2.0.0"
+}
+
+variable "region" {
+  type = string
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "tls" {
+}
+
+module "iam_example_iam-account" {
+  source  = "terraform-aws-modules/iam/aws//examples/iam-account"
+  version = "3.0.0"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.2.4"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+}
