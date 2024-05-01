cff67f8381fab43bfd3b291b9b60e44737d8bdd5
Dalzy Mendoza <dalzy@canva.com>
2024-05-01 10:36:56 +1000

    bump down s3 usage

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "iam_example_iam-account" {
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.2.4"
+  version = "3.0.1"
 
   bucket = "my-s3-bucket"
   acl    = "private"
